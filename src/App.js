c560e76c864f9775bcc38b852a24248329894afb
zaqi <zaqifathis@gmail.com>
2022-10-27 13:59:05 +0800

    setup leva

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls, ContactShadows } from "@react-three/drei";
 import { proxy, useSnapshot } from "valtio";
 import { useControls } from "leva";
-import {
-  EffectComposer,
-  Selection,
-  Outline,
-} from "@react-three/postprocessing";
 
 import Model from "./Model";
 import Picker from "./Picker";
 
 const items = {};
+const outlines = {};
 for (let i = 0; i < 58; i++) {
   if (i < 1) {
     items[`strip${i}`] = "#FF0000";
@@ -30,6 +26,16 @@ const state = proxy({
 export default function App() {
   const snap = useSnapshot(state);
   const [hovered, setHovered] = useState(null);
+  const { enable, strip, color } = useControls({
+    enable: false,
+    strip: {
+      value: 0,
+      min: 0,
+      max: 57,
+      step: 1,
+    },
+    color: "#0208ff",
+  });
 
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
@@ -45,7 +51,11 @@ export default function App() {
     }
   }, [hovered]);
 
-  const handleClick = (event, name) => {
+  useEffect(() => {
+    console.log(`update enable to: ${enable}`);
+  }, [enable]);
+
+  const handleClick = (event) => {
     event.stopPropagation();
     state.current = event.object.name;
   };
@@ -64,7 +74,7 @@ export default function App() {
   };
 
   const handleColorChange = (event) => {
-    state.items[snap.current] = event;
+    state.items[state.current] = event;
   };
 
   return (
@@ -81,16 +91,13 @@ export default function App() {
           position={[300, 1500, 800]}
         />
         <Suspense fallback={null}>
-          <Selection>
-            <Model
-              handleClick={handleClick}
-              handlePointerOver={handlePointerOver}
-              handlePointerOut={handlePointerOut}
-              handlePointerMissed={handlePointerMissed}
-              color={snap}
-            />
-          </Selection>
-
+          <Model
+            handleClick={handleClick}
+            handlePointerOver={handlePointerOver}
+            handlePointerOut={handlePointerOut}
+            handlePointerMissed={handlePointerMissed}
+            color={snap}
+          />
           <OrbitControls
             target={[0, 150, 0]}
             maxDistance={800}
